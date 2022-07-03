1d354fa84d62f34797092c20f2a12630d1ef2535
lidqqq <lidqqq.dev@gmail.com>
2022-07-03 14:22:02 +0900

    chore: define root state

--- src/lib/jotai/atom.ts
+++ src/lib/jotai/atom.ts
@@ -6,4 +6,20 @@ function key(key: string) {
 	return `${ATOM_PREFIX}${key}`;
 }
 
-export const projectsAtom = atomWithStorage<string[]>(key('projects'), []);
+export const projectsAtom = atomWithStorage<Project[]>(key('projects'), []);
+
+type StoreRoot = {
+	projects: Project[];
+	tasks: Task[];
+	record: TaskRecord[];
+};
+type Task = {
+	name: string;
+	project?: string;
+};
+type TaskRecord = {
+	taskName: string;
+	startDate: Date; // expect toISOString
+	endDate: Date; // expect toISOString
+};
+type Project = string;
